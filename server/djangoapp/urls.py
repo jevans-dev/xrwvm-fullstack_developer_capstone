559a51ceb8f751dba03ccec8ec3585e201829c6c
Jamie Evans <181521480+jevans-dev@users.noreply.github.com>
2024-11-10 08:19:59 -0500

    Create login view and logout functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,14 +2,17 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
 
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    # path for login and logout
+    path(route='login', view=views.login_user, name='login'),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('logout', views.logout_user, name='logout'),
 
     # path for dealer reviews view
 
