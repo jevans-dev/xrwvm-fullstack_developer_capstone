ef240f4e49ceef5d40ed9c521ac3eca78bcf4e55
Jamie Evans <evansjamie1122@gmail.com>
2025-01-14 10:40:56 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
     path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     #path for get_dealerships
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
